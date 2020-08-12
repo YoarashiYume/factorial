174e1c031e644f066863c74d7251090422426ab5
YoarashiYume <61472665+YoarashiYume@users.noreply.github.com>
2020-08-12 18:13:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculating the factorial of large numbers
+usage : ``` <progname> <factorialValue> ```
+where 'factorialValue' is the factorial of the number to calculate
---
+++ factorial.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+//#define DEBUG
+int main(int argc, char* argv[])
+{
+    typedef unsigned char byte;
+#ifndef DEBUG
+    if (argc != 2)
+    {
+        std::cout << "Only one argument\n";
+        return -1;
+    }
+    int factorialValue = std::atoi(argv[1]);
+#else
+    int factorialValue = 5;
+#endif
+    std::vector<byte> result = { 1 };
+    unsigned int accumulate = 0;
+    for (; factorialValue > 0; --factorialValue)
+        for (size_t i = 0; ; ++i)
+            if (i < result.size())
+            {
+                accumulate += (int)result.at(i) * factorialValue;
+                result.at(i) = accumulate % 10;
+                accumulate /= 10;
+                if (accumulate == 0 && i == result.size() - 1)
+                   break;
+            }
+            else
+            {
+                result.push_back(accumulate % 10);
+                accumulate /= 10;
+                if (accumulate == 0)
+                    break;
+            }
+    for (auto it = result.rbegin(); it != result.rend(); ++it)
+        std::cout << (int)(*it);
+    return 0;
+}
